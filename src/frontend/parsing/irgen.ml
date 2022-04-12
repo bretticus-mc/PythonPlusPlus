1bf382b29bfc2277259fc93ac839a36cad6d894d
Brett McStotts <blm2160@columbia.edu>
2022-04-12 13:16:18 -0400

    updated module context name

--- src/frontend/parsing/irgen.ml
+++ src/frontend/parsing/irgen.ml
@@ -24,14 +24,14 @@ let translate (globals, functions) =
 
   (* Create the LLVM compilation module into which
      we will generate code *)
-  let the_module = L.create_module context "MicroC" in
+  let the_module = L.create_module context "PythonPP" in
 
   (* Get types from the context *)
   let i32_t      = L.i32_type    context
   and i8_t       = L.i8_type     context
   and i1_t       = L.i1_type     context in
 
-  (* Return the LLVM type for a MicroC type *)
+  (* Return the LLVM type for a PythonPP type *)
   let ltype_of_typ = function
       A.Int   -> i32_t
     | A.Bool  -> i1_t
